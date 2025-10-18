b95ac1d74254cdcc580a1512d14c684c65b3c341
osamaperfumes <osamaperfumes@gmail.com>
2025-10-18 22:30:50 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1893376120-b8d7e",
+  "appId": "1:974385075515:web:e2203e0d340c8a9f99d31e",
+  "apiKey": "AIzaSyAbDcWVER3di6fyExB2UuuF6zUBLNlbXSQ",
+  "authDomain": "studio-1893376120-b8d7e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "974385075515"
+};
